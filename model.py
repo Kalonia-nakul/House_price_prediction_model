67899416962cbeadd56944250a935875b9875d36
Kalonia-nakul <nakulkalonia2006@gmail.com>
2026-04-19 18:10:58 +0530

    second commit

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas 
 import numpy
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score , root_mean_squared_error
+from sklearn.metrics import root_mean_squared_error , r2_score
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 
@@ -16,3 +16,11 @@ df['airconditioning'] = df['airconditioning'].map(mapping)
 df['prefarea'] = df['prefarea'].map(mapping)
 
 mapping2={'furnished' : 2 , 'semi-furnished' : 1 , 'unfurnished' : 0}
+df['furnishingstatus'] = df['furnishingstatus'].map(mapping2)
+
+x = df[['area'],['bedrooms'],['bathrooms'],['stories'],['mainroad'],['guestroom'],['basement'],['hotwaterheating'],['airconditioning'],['parking'],['prefarea'],['furnishingstatus']]
+y = df['price']
+x_train , x_test , y_train , y_test = train_test_split(x , y , train_size=0.2 , random_state=42)
+
+model = LinearRegression()
+model.fit()
